e2da2e61381f73b2e335c2c5b9a860904b3566c1
Nick Spinale <nick@nickspinale.com>
2025-11-04 15:53:16 -0800

    capDL-tool: add flake.nix for dev shell
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

---
+++ capDL-tool/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        ghcVersion = "9103";
+        hls = pkgs.haskell-language-server.override {
+          supportedGhcVersions = [ ghcVersion ];
+        };
+      in rec {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            hls
+          ] ++ (with pkgs; [
+            stack
+            libxml2
+          ]);
+        };
+      }
+    );
+}
