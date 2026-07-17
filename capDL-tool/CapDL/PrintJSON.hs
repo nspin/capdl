2a3012fe21212a013b1a8823cd34760ee4607a26
Nick Spinale <nick@nickspinale.com>
2026-07-17 13:21:40 -0700

    capDL-tool: json: add support for XN vm attributes

--- capDL-tool/CapDL/PrintJSON.hs
+++ capDL-tool/CapDL/PrintJSON.hs
@@ -313,6 +313,7 @@ data CapFrame = CapFrame
     { object :: ObjID
     , rights :: Rights
     , cached :: Bool
+    , executable :: Bool
     } deriving (Eq, Show, Generic, ToJSON, FromJSON)
 
 data CapPageTable = CapPageTable
@@ -595,10 +596,11 @@ translateCapWith translateId cap = case cap of
     C.TCBCap capObj -> Cap_Tcb $ CapTCB (translateId capObj)
     C.IRQHandlerCap capObj -> Cap_IrqHandler $ CapIRQHandler (translateId capObj)
     C.VCPUCap capObj -> Cap_VCpu $ CapVCPU (translateId capObj)
-    C.FrameCap { capObj, capRights, capCached } -> Cap_Frame $ CapFrame
+    C.FrameCap { capObj, capRights, capCached, capExecutable } -> Cap_Frame $ CapFrame
         { object = translateId capObj
         , rights = translateRights capRights
         , cached = capCached
+        , executable = capExecutable
         }
     C.PTCap capObj _ -> Cap_PageTable $ CapPageTable (translateId capObj)
     C.PDCap capObj _ -> Cap_PageTable $ CapPageTable (translateId capObj)
